b1fa8aa3d5de6b4b913be407785161198fb20ec1
m19e <nexus.me7290@gmail.com>
2020-12-01 22:19:14 +0900

    Link first-post to home

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,3 +1,14 @@
-const FirstPost = () => <h1>First Post</h1>;
+import Link from "next/link";
+
+const FirstPost = () => (
+    <>
+        <h1>First Post</h1>
+        <h2>
+            <Link href={"/"}>
+                <a>Back to home</a>
+            </Link>
+        </h2>
+    </>
+);
 
 export default FirstPost;
